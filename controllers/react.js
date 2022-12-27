ce42a8e4352a5e90b44a636952c0a401ea0d62ba
rana-arju <arjurana20@gmail.com>
2022-12-27 01:21:19 -0800

    post reaction get from database

--- controllers/react.js
+++ controllers/react.js
@@ -30,11 +30,45 @@ exports.reactPost = async (req, res) => {
 
 exports.getReacts = async (req, res) => {
   try {
-    const reacts = await React.find({ postRef: req.params.id });
-
-    // const check1 = reacts.find(
-    //   (x) => x.reactBy.toString() == req.user.id
-    // );
+    const reactsArray = await React.find({ postRef: req.params.id });
+    console.log("total", reactsArray);
+    const newReacts = reactsArray.reduce((group, react) => {
+      let key = react["react"];
+      if (!group[key]) {
+        group[key] =group[key] || [];
+        group[key].push(react);
+        return group;
+        
+      }
+    },{})
+    const reacts = [
+      {
+        react: "like",
+        count: newReacts.like ? newReacts.like.length : 0,
+      },
+      {
+        react: "haha",
+        count: newReacts.haha ? newReacts.haha.length : 0,
+      },
+      {
+        react: "love",
+        count: newReacts.love ? newReacts.love.length : 0,
+      },
+      {
+        react: "sad",
+        count: newReacts.sad ? newReacts.sad.length : 0,
+      },
+      {
+        react: "wow",
+        count: newReacts.wow ? newReacts.wow.length : 0,
+      },
+      {
+        react: "angry",
+        count: newReacts.angry ? newReacts.angry.length : 0,
+      },
+    ];
+   
+    console.log(newReacts);
 
     const check = await React.findOne({
       postRef: req.params.id,
@@ -43,6 +77,7 @@ exports.getReacts = async (req, res) => {
     res.json({
       reacts,
       check: check?.react,
+      total: reactsArray.length
    
     });
   } catch (error) {
