12dc6dd71a7eb11bd84aaabf8f5b2f39e9df4248
rana-arju <arjurana20@gmail.com>
2022-12-30 10:10:45 -0800

    cors policy problem fix

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ require("dotenv").config();
 var cors = require("cors");
 const { readdirSync } = require("fs");
 const port = process.env.PORT || 5000;
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 app.use(fileUpload({ useTempFiles: true }));
