12c5425be0fcb2fb5a0104a98fe84a3fc0c4e166
rana-arju <arjurana20@gmail.com>
2023-01-01 08:48:05 -0800

    cors option add on index file

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ require("dotenv").config();
 var cors = require("cors");
 const { readdirSync } = require("fs");
 const port = process.env.PORT || 5000;
-app.use(cors());
+
+const corsOptions = {
+  origin: "https://daily-connect.vercel.app",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+
 app.use(express.json());
 
 app.use(fileUpload({ useTempFiles: true }));
