13414c8c9c5ea446005cac56bb2d552955f026ad
rana-arju <arjurana20@gmail.com>
2023-01-03 08:08:38 -0800

    update

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ var cors = require("cors");
 const { readdirSync } = require("fs");
 const port = process.env.PORT || 5000;
 
-const corsOptions = {
-  methods: ["GET", "POST", "PUT", "PATCH", "HEAD", "DELETE"],
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   methods: ["GET", "POST", "PUT", "PATCH", "HEAD", "DELETE"],
+//   origin: "*",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
+app.use(cors());
 
 app.use(express.json());
 
