e479ff96a2c033708a44f0670bc2e7b847314b75
rana-arju <arjurana20@gmail.com>
2023-01-06 03:03:21 -0800

    update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const corsOptions = {
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
-app.use(cors({ corsOptions }));
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
